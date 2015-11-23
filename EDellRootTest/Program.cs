bb7b7326e70c75359e72b7215a17d33d797e68b6
Kristof Mattei <Kristof Mattei>
2015-11-23 19:31:09 +0100

    Initial commit

---
+++ EDellRootTest/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace EDellRootTest
+{
+    using System;
+    using System.Security.Principal;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var id = WindowsIdentity.GetCurrent();
+            var isAdmin = id != null && id.Owner != id.User;
+
+            Console.WriteLine(isAdmin);
+        }
+    }
+}
